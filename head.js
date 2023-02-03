ac3e0ddb4fffd662c9c0bb12256d9b3214ea0c58
Thasia Magalhaes <thasia.oliveira@gmail.com>
2023-02-03 06:32:46 -0500

    Head

---
+++ head.js
@@ -0,0 +1,10 @@
+const head = array => array[0] || undefined;
+
+const assertEqual = (actual, expected) => {
+console.log(actual === expected ? "Assertion Passed" : "Assertion Failed");
+};
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([1]), 1);
+assertEqual(head([]), undefined);
