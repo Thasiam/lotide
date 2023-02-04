aea9a485123656a28e02a26d65d5d7ea2b03b96e
Thasia Magalhaes <thasia.oliveira@gmail.com>
2023-02-04 17:58:04 -0500

    flatten

---
+++ flatten.js
@@ -0,0 +1,42 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+     return console.log(`Assertion Passed: [actual] === [expected]`);
+   } else {
+     return console.log(`Assertion Failed: [actual] !== [expected]`);
+   }
+ }
+   const eqArrays = function(array1, array2) {
+   if (array1.length !== array2.length) {
+     return false;
+   }
+ 
+   for (let i = 0; i < array1.length; i++) {
+     if (array1[i] !== array2[i]) {
+       return false;
+     }
+   }
+   return true;
+ };
+ assertEqual (eqArrays([1, 2, 3], [1, 2, 3]), true);
+ assertEqual (eqArrays([1, 2, 3], [3, 2, 1]), false);
+ eqArrays([1, 2, 3], [1, 2, 3]) // => true
+ eqArrays([1, 2, 3], [3, 2, 1]) // => false
+ 
+ assertEqual (eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+ assertEqual (eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+ eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+ eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+
+const assertArraysEqual = function(array1, array2) {
+  //Step 1: Check if both arrays are equal
+  let arraysAreEqual = eqArrays(array1, array2);
+
+  //Step 2: If both arrays are equal, print "Assertion Passed" message
+  if (arraysAreEqual) {
+    console.log(`Assertion Passed: ${array1} === ${array2}`);
+
+  //Step 3: If both arrays are not equal, print "Assertion Failed" message
+  } else {
+    console.log(`Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
