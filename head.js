4a0e816e836272ed988e1197a5b640403a83d6b4
Thasia Magalhaes <thasia.oliveira@gmail.com>
2023-02-04 17:43:11 -0500

    assertArrays

--- head.js
+++ head.js
@@ -1,7 +1,7 @@
 const head = array => array[0] || undefined;
 
 const assertEqual = (actual, expected) => {
-console.log(actual === expected ? "Assertion Passed" : "Assertion Failed");
+  console.log(actual === expected ? "Assertion Passed" : "Assertion Failed");
 };
 
 assertEqual(head([5,6,7]), 5);
