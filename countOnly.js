e35fe964da71043213ca5d6aac64c27b73adacba
Thasia Magalhaes <thasia.oliveira@gmail.com>
2023-02-06 18:53:45 -0500

    countOnly

---
+++ countOnly.js
@@ -0,0 +1,47 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return console.log(`Assertion Passed: [actual] === [expected]`);
+  } else {
+    return console.log(`Assertion Failed: [actual] !== [expected]`);
+  }
+};
+
+// TEST CODE
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+
+const countOnly = function(allItems, itemsToCount) {
+  const registry  = {} 
+  for (let i = 0; i < allItems.length; i++){
+    const item = allItems[i]; 
+    if (itemsToCount[item] ===true){
+      if (registry[item]){
+ registry[item] +=1
+
+      }
+  else { registry[item] = 1}
+    }
+  }
+  console.log(registry)
+  return registry
+}
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
+
