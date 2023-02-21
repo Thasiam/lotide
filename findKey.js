8878908a6072e498c64ad01602185766f8c1b754
Thasia Magalhaes <thasia.oliveira@gmail.com>
2023-02-21 10:27:43 -0500

    findKey

---
+++ findKey.js
@@ -0,0 +1,35 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return console.log(`Assertion Passed: [actual] === [expected]`);
+  } else {
+    return console.log(`Assertion Failed: [actual] !== [expected]`);
+  }
+};
+
+
+
+module.exports = assertEqual;
+
+const findKey = function(object, callback) {
+  for (let key in object) {
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+// Test cases
+const restaurants = {
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+};
+
+assertEqual(findKey(restaurants, x => x.stars === 2), "noma");
+assertEqual(findKey(restaurants, x => x.stars === 3), "Akaleri");
+assertEqual(findKey(restaurants, x => x.stars === 4), undefined);
+assertEqual(findKey(restaurants, x => x.name === "noma"), undefined);
