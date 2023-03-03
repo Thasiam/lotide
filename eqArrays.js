40778a29e233146ab1e72aef6a7845ffb982d804
Thasia Magalhaes <thasia.oliveira@gmail.com>
2023-03-03 15:04:07 -0500

    eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,10 @@
 const assertEqual = function(actual, expected) {
- if (actual === expected) {
-    return console.log(`Assertion Passed: [actual] === [expected]`);
+  if (actual === expected) {
+    return console.log(`Assertion Passed: ${actual} === ${expected}`);
   } else {
-    return console.log(`Assertion Failed: [actual] !== [expected]`);
+    return console.log(`Assertion Failed: ${actual} !== ${expected}`);
   }
-}
+};
   const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     return false;
