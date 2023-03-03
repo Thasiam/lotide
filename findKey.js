3ac2511d691620cae87bd60d537b727b23a25f0d
Thasia Magalhaes <thasia.oliveira@gmail.com>
2023-03-03 15:05:33 -0500

    findKey

--- findKey.js
+++ findKey.js
@@ -1,14 +1,10 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    return console.log(`Assertion Passed: [actual] === [expected]`);
+    return console.log(`Assertion Passed: ${actual} === ${expected}`);
   } else {
-    return console.log(`Assertion Failed: [actual] !== [expected]`);
+    return console.log(`Assertion Failed: ${actual} !== ${expected}`);
   }
 };
-
-
-
-module.exports = assertEqual;
 
 const findKey = function(object, callback) {
   for (let key in object) {
