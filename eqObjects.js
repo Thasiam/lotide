6ffcfaf52f6ad904ee16e3f680065576464f6a90
Thasia Magalhaes <thasia.oliveira@gmail.com>
2023-03-03 15:04:55 -0500

    eqObjects

--- eqObjects.js
+++ eqObjects.js
@@ -1,11 +1,10 @@
-const assertEqual = function(actual, expected) {
+cconst assertEqual = function(actual, expected) {
   if (actual === expected) {
-    return console.log(`Assertion Passed: [actual] === [expected]`);
+    return console.log(`Assertion Passed: ${actual} === ${expected}`);
   } else {
-    return console.log(`Assertion Failed: [actual] !== [expected]`);
+    return console.log(`Assertion Failed: ${actual} !== ${expected}`);
   }
 };
-
 // TEST CODE
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
