cdf5a30800bf0c9676d7e456dedd69ef0429bcfd
Thasia Magalhaes <thasia.oliveira@gmail.com>
2023-03-04 14:21:53 -0500

    eqObjects

--- eqObjects.js
+++ eqObjects.js
@@ -1,17 +1,24 @@
-cconst assertEqual = function(actual, expected) {
+const assertEqual = function(actual, expected) {
   if (actual === expected) {
     return console.log(`Assertion Passed: ${actual} === ${expected}`);
   } else {
     return console.log(`Assertion Failed: ${actual} !== ${expected}`);
   }
 };
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+const eqArrays = function(actual, expected) {
+  if (actual.length !== expected.length) {
+    return false;
+  } else {
+    for (let i = 0; i < actual.length; i++) {
+      if (actual[i] !== expected[i]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
 
-// Returns true if both objects have identical keys with identical values.
-// Otherwise you get back a big fat false!
-//this function checks if two objects have the same set of keys and if the values of each key in both objects are the same. If both conditions are met, the function returns true. Otherwise, it returns false.
+// Implement the definition for function eqObjects which will take in two objects and returns true or false, based on a perfect match.
 
 const eqObjects = function(obj1, obj2) {
   const keys1 = Object.keys(obj1);
@@ -21,10 +28,35 @@ const eqObjects = function(obj1, obj2) {
   }
 
   for (let key of keys1) {
-    if (obj1[key] !== obj2[key]) {
-      return false;
-    }
+      if(Array.isArray(obj1[key]) && Array.isArray(obj2[key])) {
+        if(!eqArrays(obj1[key], obj2[key])) return false
+      }
+
+      if (obj1[key] !== obj2[key]) {
+        return false;
+      }
   }
 
   return true;
 };
+
+/*TEST CODE*/
+
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
+
+const shirtObject = { color: "red", size: "medium" };
+const anotherShirtObject = { size: "medium", color: "red" };
+eqObjects(shirtObject, anotherShirtObject); // => true
+
+const longSleeveShirtObject = { size: "medium", color: "red", sleeveLength: "long" };
+eqObjects(shirtObject, longSleeveShirtObject); // => false
+
+const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
+const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
+eqObjects(multiColorShirtObject, anotherMultiColorShirtObject); // => true
+
+const longSleeveMultiColorShirtObject = { size: "medium", colors: ["red", "blue"], sleeveLength: "long" };
+eqObjects(multiColorShirtObject, longSleeveMultiColorShirtObject); // => false
+
+
